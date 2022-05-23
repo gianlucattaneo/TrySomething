616dca7c4ee660d72f48658886da652e2a612280
Gianluca <gianlucattaneo00@gmail.com>
2022-05-24 00:57:46 +0200

    Modifiche albero per test.csv

--- DecisionTrees.py
+++ DecisionTrees.py
@@ -5,6 +5,7 @@ from sklearn.inspection import DecisionBoundaryDisplay
 
 import matplotlib.pyplot as plt
 import numpy as np
+import csv
 
 
 def test():
@@ -62,6 +63,58 @@ def gennaro():
 
 
 if __name__ == '__main__':
-    gennaro()
+    feature_names = ['cart_count', 'https', 'meta_count', 'price_count']
+    target_names = ['authorized', 'unauthorized']
+    features = []
+    targets = []
+    with open('Stats/total.csv', mode='r') as infile:
+        reader = csv.DictReader(infile, delimiter=';')
+        print(reader.fieldnames)
+        for row in reader:
+            tmp = []
+            for feat in feature_names:
+                tmp.append(row[feat])
+            features.append(tmp)
+            targets.append(row['class'])
+
+    n_features = len(np.unique(feature_names))
+    n_classes = len(np.unique(target_names))
+    plot_colors = 'ryb'
+    plot_step = 0.02
+
+    for pairidx, pair in enumerate([[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]]):
+        X = features[:, pair]
+        y = targets
+
+        clf = tree.DecisionTreeClassifier().fit(X, y)
+
+        ax = plt.subplot(2, 3, pairidx + 1)
+        plt.tight_layout(h_pad=0.5, w_pad=0.5, pad=2.5)
+        DecisionBoundaryDisplay.from_estimator(
+            clf,
+            X,
+            cmap=plt.cm.RdYlBu,
+            response_method="predict",
+            ax=ax,
+            xlabel=feature_names[pair[0]],
+            ylabel=feature_names[pair[1]],
+        )
+
+        for i, color in zip(range(n_classes), plot_colors):
+            idx = np.where(y == i)
+            plt.scatter(
+                X[idx, 0],
+                X[idx, 1],
+                c=color,
+                # label=iris.target_names[i],
+                cmap=plt.cm.RdYlBu,
+                edgecolor="black",
+                s=15,
+            )
+
+    plt.suptitle("Decision surface of decision trees trained on pairs of features")
+    plt.legend(loc="lower right", borderpad=0, handletextpad=0)
+    _ = plt.axis("tight")
+    plt.show()
 
 
